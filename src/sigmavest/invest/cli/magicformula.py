68018845c9f0bb15cc6eebe73ac06b4f79862fa9
DESKTOP-8ISDEH9\ivang <ivan.georgiev@gmail.com>
2025-06-15 23:04:32 +0300

    magicformula scraper

--- src/sigmavest/invest/cli/magicformula.py
+++ src/sigmavest/invest/cli/magicformula.py
@@ -3,6 +3,7 @@ from rich.console import Console
 
 from sigmavest.dependency import resolve
 from sigmavest.invest.adapter.magicformula.magic_formula_scraper import MagicFormulaScraper
+from sigmavest.invest.service.database import DatabaseService
 from sigmavest.settings import Settings
 
 app = typer.Typer()
@@ -14,7 +15,8 @@ settings = resolve(Settings)
 def scrape(
     username: str = typer.Option(None, "--username", "-u", help="Username."),
     password: str = typer.Option(None, "--password", "-p", help="User password."),
-    output: str = typer.Option(None, "--output-file", "-o", help="Output file name."),
+    output_file: str = typer.Option(None, "--output-file", "-o", help="Output file name."),
+    output_table: str = typer.Option(None, "--output-table", "-t", help="Output table name."),
     min_market_cap: int = typer.Option(
         settings.MAGIC_FORMULA_MIN_MARKET_CAP, "--min-market-cap", "-m", min=1, help="Miniumum market capitalization."
     ),
@@ -27,11 +29,13 @@ def scrape(
             raise ValueError("User credentials are not provided.")
         scraper = MagicFormulaScraper(username, password)
         results = scraper.scrape(min_market_cap)
-        if output:
-            results.to_csv(output)
-            console.print(f"[green]Results exported to '{output}'[/green]")
-        else:
-            console.print(results)
+        if output_file:
+            results.to_csv(output_file)
+            console.print(f"[green]Results exported to '{output_file}'[/green]")
+        if output_table:
+            service = resolve(DatabaseService)
+            results.to_sql(output_table, service.db.db)
+        console.print(results)
     except ValueError as e:
         console.print(f"[red]Error: {e}[/red]")
         raise typer.Exit(code=1)
